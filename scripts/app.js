d63e1c748c5f85b041f58f1d85261438cda09ac9
Jpdl925 <jtpg925@gmail.com>
2024-04-25 21:25:16 -0700

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,54 @@
+// Button used for fetching, and changing the text on screen to the randomly generated dad joke
+let generalKnowledge = document.getElementById("generalKnowledge").addEventListener('click', () =>{
+    triviaQuestion();
+});
+let scienceNature = document.getElementById("scienceNature").addEventListener('click', () =>{
+    scienceNatureQuestion();
+});
+let art = document.getElementById("art").addEventListener('click', () =>{
+    artQuestion();
+});
+// Text that is changed from the button click to a randomly generated dad joke from the api
+let questionTxt = document.getElementById("questionTxt");
+let answerTxt = document.getElementById("answer");
+// Laugh track that plays once button is pressed
+let audio = new Audio("/media/laughTrack.mp3");
+
+
+
+
+
+async function triviaQuestion(){
+    let firstQ = await fetch("https://opentdb.com/api.php?amount=10&category=9").then((Response) => Response.json());
+    let question = firstQ.results[0].question;
+    let answer = firstQ.results[0].correct_answer;
+    questionTxt.innerText = question;
+    answerTxt.innerText = answer;
+    console.log(firstQ.results);
+    console.log(question);
+    console.log(answer);
+}
+async function scienceNatureQuestion(){
+    let secondQ = await fetch("https://opentdb.com/api.php?amount=10&category=17").then((Response) => Response.json());
+    let question2 = secondQ.results[0].question;
+    let answer2 = secondQ.results[0].correct_answer;
+    questionTxt.innerText = question2;
+    answerTxt.innerText = answer2;
+    console.log(secondQ.results);
+    console.log(question2);
+    console.log(answer2);
+}
+
+async function artQuestion(){
+    let thirdQ = await fetch("https://opentdb.com/api.php?amount=10&category=25").then((Response) => Response.json());
+    let question3 = thirdQ.results[0].question;
+    let answer3 = thirdQ.results[0].correct_answer;
+    questionTxt.innerText = question3;
+    answerTxt.innerText = answer3;
+    console.log(thirdQ.results);
+    console.log(question3);
+    console.log(answer3);
+
+}
+
+
